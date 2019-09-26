a04a969acf352bcd9bc9d57bbf114759521c4b2a
Martin Moene <martin.moene@gmail.com>
2019-09-26 20:10:31 +0200

    Move option -O3, use AVR_DUDE_ROOT environment variable for programmer

--- example/avr/avr_blink_led_e.bat
+++ example/avr/avr_blink_led_e.bat
@@ -1,19 +1,23 @@
-@ :: avr_blink_led_e.bat
+@ :: avr_blink_led_e.bat [program]
+@ :: Avr-Ada project: https://sourceforge.net/projects/avr-ada/
 
-@echo.
-@echo Compile to .elf binary:
-%AVR_ADA_ROOT%\bin\avr-gnatmake -gnatyN -p -XMCU=atmega328p -Pavr_blink_led_e.gpr -cargs -O3
+@set basename=avr_blink_led_e
 
 @echo.
 @echo Compile to .s assembly (for inspection only):
-%AVR_ADA_ROOT%\bin\avr-gnatmake -gnatyN -c -p -XMCU=atmega328p -Pavr_blink_led_e.gpr -cargs -S -O3
+%AVR_ADA_ROOT%\bin\avr-gnatmake -gnatyN -c -p -O3 -XMCU=atmega328p -P%basename%.gpr -cargs -S
+
+@echo.
+@echo Compile to .elf binary:
+%AVR_ADA_ROOT%\bin\avr-gnatmake -gnatyN -p -O3 -XMCU=atmega328p -P%basename%.gpr 
 
 @echo.
 @echo Convert .elf to .hex:
-%AVR_ADA_ROOT%\bin\avr-objcopy -O ihex -R .eeprom bin\avr_blink_led_e.elf bin\avr_blink_led_e.hex
-%AVR_ADA_ROOT%\bin\avr-size bin\avr_blink_led_e.hex
+%AVR_ADA_ROOT%\bin\avr-objcopy -O ihex -R .eeprom bin\%basename%.elf bin\%basename%.hex
+%AVR_ADA_ROOT%\bin\avr-size bin\%basename%.hex
 
+@if not [%1] == [program] goto :EOF
 @echo.
 @echo Program device:
 @pause
-%AVR_ADA_ROOT%\bin\avrdude -v -patmega328p -cusbtiny -Uflash:w:bin\avr_blink_led_e.hex:i
+%AVR_DUDE_ROOT%\bin\avrdude -v -patmega328p -cusbtiny -Uflash:w:bin\%basename%.hex:i
